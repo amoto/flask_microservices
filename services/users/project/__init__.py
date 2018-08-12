88c5dea9b611015bfa820c3ad47c16b083955914
Julian Devia <judadese@gmail.com>
2018-08-12 01:11:40 -0500

    creación de db y despliegue con docker, conexión con db y pruebas

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,18 +1,30 @@
 # services/users/project/__init__.py
 
-
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy  # new
 
+# instantiate the db
+db = SQLAlchemy()
 
-# instantiate the app
-app = Flask(__name__)
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
